5562cf5bb41b4e0feac40c0bbf970754c1fa8f05
ThienNguyen <108439170+unclethien@users.noreply.github.com>
2024-06-18 22:30:38 -0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
       sans: ["Inter", "sans-serif"],
       stock: ["Stockholm", "sans-serif"],
     },
+    animation: {
+      'loop-scroll': 'loop-scroll 50s linear infinite',
+    },
+    keyframes: {
+      'loop-scroll': {
+        from: { transform: 'translateX(0)' },
+        to: { transform: 'translateX(-100%)' },
+      },
+    },
   },
   plugins: [],
 }
